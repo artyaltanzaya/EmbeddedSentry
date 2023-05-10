d5c310591c534728f52e3d8ef3179451720803bc
artyaltanzaya <aa4928@nyu.edu>
2023-05-10 00:57:02 -0400

    notebook

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,8 @@
 DigitalOut led(LED3);
 
 // Define button input pins
-// Define button input pins
 DigitalIn enterKeyButton(PB_14);
 DigitalIn recordButton(PA_0);
-
-
 
 // Define accelerometer object
 AnalogIn x_axis(PF_10);
@@ -24,6 +21,9 @@ int movementSize = 0;
 #define TOLERANCE 0.1
 #define TOLERANCE_RECORD 0.2
 
+// Define state variables
+enum State {IDLE, RECORDING, REPLAYING};
+State state = IDLE;
 
 // Function to read accelerometer data and update LED status
 void updateAccelerometer()
@@ -32,55 +32,68 @@ void updateAccelerometer()
     int16_t y = y_axis.read()*100;
     int16_t z = z_axis.read()*100;
 
-    // Detect movement for recording or replaying
-    if (recordButton.read() == 1 && movementSize < MAX_MOVEMENT_SIZE)
+    switch(state)
     {
-        movementBuffer[movementSize][0] = x;
-        movementBuffer[movementSize][1] = y;
-        movementBuffer[movementSize][2] = z;
-        movementSize++;
-    }
-    else if (enterKeyButton.read() == 1 && movementSize > 0)
-    {
-        bool success = true;
-        for (int i = 0; i < movementSize; i++)
-        {
-            int16_t xDiff = abs(x - movementBuffer[i][0]);
-            int16_t yDiff = abs(y - movementBuffer[i][1]);
-            int16_t zDiff = abs(z - movementBuffer[i][2]);
-            if (xDiff > TOLERANCE_RECORD || yDiff > TOLERANCE_RECORD || zDiff > TOLERANCE_RECORD)
+        case IDLE:
+            if (recordButton.read() == 1)
             {
-                success = false;
-                break;
+                // Start recording
+                state = RECORDING;
+                movementSize = 0;
             }
-        }
-        if (success)
-        {
-            led = 1;
-            wait_us(0.5);
-            led = 0;
-        }
-    }
-    else if (movementSize > 0)
-    {
-        bool success = true;
-        for (int i = 0; i < movementSize; i++)
-        {
-            int16_t xDiff = abs(x - movementBuffer[i][0]);
-            int16_t yDiff = abs(y - movementBuffer[i][1]);
-            int16_t zDiff = abs(z - movementBuffer[i][2]);
-            if (xDiff > TOLERANCE || yDiff > TOLERANCE || zDiff > TOLERANCE)
+            break;
+
+        case RECORDING:
+            if (recordButton.read() == 0)
             {
-                success = false;
-                break;
+                // Stop recording
+                state = IDLE;
             }
-        }
-        if (success)
-        {
-            led = 1;
-            wait_us(0.5);
-            led = 0;
-        }
+            else if (movementSize < MAX_MOVEMENT_SIZE)
+            {
+                // Record movement
+                movementBuffer[movementSize][0] = x;
+                movementBuffer[movementSize][1] = y;
+                movementBuffer[movementSize][2] = z;
+                movementSize++;
+            }
+            break;
+
+        case REPLAYING:
+            if (enterKeyButton.read() == 0)
+            {
+                // Stop replaying
+                state = IDLE;
+            }
+            else if (movementSize > 0)
+            {
+                // Check if current movement matches recorded movement
+                bool success = true;
+                for (int i = 0; i < movementSize; i++)
+                {
+                    int16_t xDiff = abs(x - movementBuffer[i][0]);
+                    int16_t yDiff = abs(y - movementBuffer[i][1]);
+                    int16_t zDiff = abs(z - movementBuffer[i][2]);
+                    if (xDiff > TOLERANCE || yDiff > TOLERANCE || zDiff > TOLERANCE)
+                    {
+                        success = false;
+                        break;
+                    }
+                }
+
+                if (success)
+                {
+                    // Indicate successful unlock
+                    led = 1;
+                    wait_us(500000);
+                    led = 0;
+                }
+            }
+            break;
+
+        default:
+            state = IDLE;
+            break;
     }
 }
 
@@ -90,11 +103,20 @@ int main()
     while (true)
     {
         updateAccelerometer();
-        // Print out movements in buffer
-        printf("Recorded Movements:\n");
-        for (int i = 0; i < movementSize; i++)
+
+        // Check if enter key is pressed to start replaying movement
+        if (enterKeyButton.read() == 1 && state == IDLE && movementSize > 0)
         {
-            printf("X: %d, Y: %d, Z: %d\n", movementBuffer[i][0], movementBuffer[i][1], movementBuffer[i][2]);
+            state = REPLAYING;
+            printf("Replaying Movement %d:\n", movementSize);
+            printf("State %d:\n", state);
+        }
+
+        // Print out movements in buffer during recording
+        if (state == RECORDING)
+        {
+            printf("Recording Movement %d:\n", movementSize);
+            printf("X: %d, Y: %d, Z: %d\n", movementBuffer[movementSize-1][0], movementBuffer[movementSize-1][1], movementBuffer[movementSize-1][2]);
         }
     }
 }
